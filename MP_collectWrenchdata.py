a62721430a88562d5b5ab1f601908967dbee7ff2
physics-constrained-Real2Sim <physics-constrained-Real2Sim>
2026-06-26 20:22:32 +0800

    update native wrenchsim

--- MP_collectWrenchdata.py
+++ MP_collectWrenchdata.py
@@ -37,7 +37,7 @@ from multiprocessing.connection import Connection
 from pybullet_utils import bullet_client
 from tqdm import tqdm
 
-from WrenchSim import WrenchSim
+from WrenchSim_equidistant_eye import WrenchSim
 from episode_writer import EpisodeWriter
 
 
@@ -99,7 +99,7 @@ def collect_one_episode(
         while not sim.done and sim_step < max_steps:
             should_record = sim_step % record_every_n_sim_steps == 0
             if should_record:
-                obs = sim.collect_observation(direct=False)
+                obs = sim.collect_observation(direct=False, use_eye_in_hand= False)
 
             sim.step()
 
@@ -165,8 +165,9 @@ def collect_episode_in_worker(
         env_mesh_path= "./data/background/repaired_table/tabletop.obj",
         manipulated_obj_path= "./data/objects/screw/screw.obj",
         manipulated_obj_collision_path = "./data/objects/screw/screw_collision_asset.obj",
+        wrench_mesh_path = "data/objects/wrench/wrench_v2/wrench_attached_v2.obj",
         clipper_obj_path   = "data/objects/clipper/clipper.obj",
-        initial_grasp_path = "data/objects/wrench/wrench_engage.yaml",
+        initial_grasp_path = "data/objects/wrench/wrench_v2/wrench_engage.yaml",
         if_FPSA = False,
         fpsa_aug_root = "./data/objects/bracket/fpsa_aug_outputs",
         fpsa_include_base = True,
@@ -183,14 +184,17 @@ def collect_episode_in_worker(
         randomize_wrenchpose = True,
         wrench_xyz_jitter = 0.01,
         wrench_y_euler_jitter= 0.00,
-        randomize_objpose  = True,
-        obj_x_jit    = 0.06,
+        randomize_objpose  = False,
+        obj_x_jit    = 0.12,
         obj_y_jit    = 0.1,
         obj_z_jit    = 0.05,
         obj_z_eul_jit = 0.0,
         randomize_campose = True,
         cam_xyz_jit  = 0.01,
         cam_eul_jit  = 0.005,
+        randomize_fisheye_cam = True,
+        fisheye_eyz_jit = 0.005,
+        fisheye_eul_jit = 0.002,
         randomize_image_noise= True,
         randomize_object_color = True,
         object_color_mode = "bounded",  # "bounded" or "recolor"
@@ -201,8 +205,10 @@ def collect_episode_in_worker(
         randomize_distractors= True,
         distractor_root= "/mnt/storage/GoogleScannedObjects",
         distractor_num_range= (0, 5),
-        distractor_target_size_range= (0.06, 0.4),
-        distractor_workspace = ((-0.2, 0.8), (-0.72, 0.42)),
+        distractor_target_size_range= (0.1, 0.5),
+        distractor_workspace = ((-0.2, 1.3), (-0.72, 0.42)),
+        distractor_clearance = 0.07,
+        distractor_path_clearance = 0.07,
         # at least 10 pixel of the target object
         distractor_min_target_mask_pixels= 10,
     )
